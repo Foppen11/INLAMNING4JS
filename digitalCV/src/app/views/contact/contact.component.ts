d5df21d37f662db19e5d061799354ee331260889
Max Forsberg <max.forsberg@outlook.com>
2021-07-06 14:25:46 +0200

    new

--- digitalCV/src/app/views/contact/contact.component.ts
+++ digitalCV/src/app/views/contact/contact.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageModel } from 'src/app/module/message/message-model'; 
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +10,16 @@ export class ContactComponent implements OnInit {
   
   public user = new MessageModel
 
-  constructor(private router: Router) {}
 
   ngOnInit(): void {
     
   }
 
-
   onSubmit(regForm: any) {
-    console.log(this.user)
-    this.user.name = ''
-    this.user.email = ''
-    this.user.title = ''
-    this.user.message = ''
+    console.log(regForm.form.value)
+    regForm.reset()
     alert('Thanks for your message')
   }
+
+
 }
